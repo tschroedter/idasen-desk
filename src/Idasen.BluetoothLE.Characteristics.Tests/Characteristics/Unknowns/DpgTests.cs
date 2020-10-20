e9027951d7765453857f8dd4e3596083542aae74
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-21 09:06:57 +1030

    add DpgTests for Unknown

---
+++ src/Idasen.BluetoothLE.Characteristics.Tests/Characteristics/Unknowns/DpgTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions ;
+using Idasen.BluetoothLE.Characteristics.Characteristics.Unknowns ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+
+namespace Idasen.BluetoothLE.Characteristics.Tests.Characteristics.Unknowns
+{
+    [ TestClass ]
+    public class DpgTests
+    {
+        [ TestMethod ]
+        public void Constructor_ForInvoked_SetsRawDpg ( )
+        {
+            CreateSut ( ).RawDpg
+                         .Should ( )
+                         .BeEmpty ( ) ;
+        }
+
+        private Dpg CreateSut ( )
+        {
+            return new Dpg ( ) ;
+        }
+    }
+}
