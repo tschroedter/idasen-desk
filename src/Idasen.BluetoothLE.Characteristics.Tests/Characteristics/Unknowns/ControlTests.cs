300307808bee065825574c0e5328047595082080
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-21 09:35:07 +1030

    add ControlTests for Unknown

---
+++ src/Idasen.BluetoothLE.Characteristics.Tests/Characteristics/Unknowns/ControlTests.cs
@@ -0,0 +1,42 @@
+﻿using System ;
+using System.Threading.Tasks ;
+using FluentAssertions ;
+using Idasen.BluetoothLE.Characteristics.Characteristics.Unknowns ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+
+namespace Idasen.BluetoothLE.Characteristics.Tests.Characteristics.Unknowns
+{
+    [ TestClass ]
+    public class ControlTests
+    {
+        [ TestMethod ]
+        public void Constructor_ForInvoked_SetsRawControl2 ( )
+        {
+            CreateSut ( ).RawControl2
+                         .Should ( )
+                         .BeEmpty ( ) ;
+        }
+
+        [ TestMethod ]
+        public void Constructor_ForInvoked_SetsRawControl3 ( )
+        {
+            CreateSut ( ).RawControl3
+                         .Should ( )
+                         .BeEmpty ( ) ;
+        }
+
+        [ TestMethod ]
+        public async Task TryWriteRawControl2_ForInvoked_ReturnsFalse ( )
+        {
+            var result = await CreateSut ( ).TryWriteRawControl2 ( Array.Empty < byte > ( ) ) ;
+
+            result.Should ( )
+                  .BeFalse ( ) ;
+        }
+
+        private Control CreateSut ( )
+        {
+            return new Control ( ) ;
+        }
+    }
+}
