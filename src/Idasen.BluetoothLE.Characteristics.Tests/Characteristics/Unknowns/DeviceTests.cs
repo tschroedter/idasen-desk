e7054889e218f9daf13bf46000b41eb9d61959c3
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-21 09:57:34 +1030

    add DeviceTests for Unknown

---
+++ src/Idasen.BluetoothLE.Characteristics.Tests/Characteristics/Unknowns/DeviceTests.cs
@@ -0,0 +1,100 @@
+﻿using System ;
+using Windows.Devices.Bluetooth ;
+using Windows.Devices.Bluetooth.GenericAttributeProfile ;
+using FluentAssertions ;
+using Idasen.BluetoothLE.Characteristics.Characteristics.Unknowns ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+
+namespace Idasen.BluetoothLE.Characteristics.Tests.Characteristics.Unknowns
+{
+    [ TestClass ]
+    public class DeviceTests
+    {
+        [ TestMethod ]
+        public void Constructor_ForInvoked_SetsConnectionStatusChanged ( )
+        {
+            CreateSut ( ).ConnectionStatusChanged
+                         .Should ( )
+                         .BeNull ( ) ;
+        }
+
+        [ TestMethod ]
+        public void Constructor_ForInvoked_SetsGattServicesRefreshed ( )
+        {
+            CreateSut ( ).GattServicesRefreshed
+                         .Should ( )
+                         .BeNull ( ) ;
+        }
+
+        [ TestMethod ]
+        public void Constructor_ForInvoked_SetsGattCommunicationStatus ( )
+        {
+            CreateSut ( ).GattCommunicationStatus
+                         .Should ( )
+                         .Be ( GattCommunicationStatus.Unreachable ) ;
+        }
+
+        [ TestMethod ]
+        public void Constructor_ForInvoked_SetsName ( )
+        {
+            CreateSut ( ).Name
+                         .Should ( )
+                         .Be ( Device.UnknownDeviceName ) ;
+        }
+
+        [ TestMethod ]
+        public void Constructor_ForInvoked_SetsDeviceId ( )
+        {
+            CreateSut ( ).DeviceId
+                         .Should ( )
+                         .Be ( Device.UnknownDeviceId ) ;
+        }
+
+        [ TestMethod ]
+        public void Constructor_ForInvoked_SetsIsPaired ( )
+        {
+            CreateSut ( ).IsPaired
+                         .Should ( )
+                         .BeFalse ( ) ;
+        }
+
+        [ TestMethod ]
+        public void Constructor_ForInvoked_SetsGattServices ( )
+        {
+            CreateSut ( ).GattServices
+                         .Should ( )
+                         .BeEmpty ( ) ;
+        }
+
+        [ TestMethod ]
+        public void Constructor_ForInvoked_SetsConnectionStatus ( )
+        {
+            CreateSut ( ).ConnectionStatus
+                         .Should ( )
+                         .Be ( BluetoothConnectionStatus.Disconnected ) ;
+        }
+
+        [ TestMethod ]
+        public void Connect_ForInvoked_DoesNothing ( )
+        {
+            Action action = ( ) => CreateSut ( ).Connect ( ) ;
+
+            action.Should ( )
+                  .NotThrow < Exception > ( ) ;
+        }
+
+        [TestMethod]
+        public void Dispose_ForInvoked_DoesNothing()
+        {
+            Action action = () => CreateSut().Dispose (  );
+
+            action.Should()
+                  .NotThrow<Exception>();
+        }
+
+        private Device CreateSut ( )
+        {
+            return new Device ( ) ;
+        }
+    }
+}
--- src/Idasen.BluetoothLE.Characteristics/Characteristics/Unknowns/Device.cs
+++ src/Idasen.BluetoothLE.Characteristics/Characteristics/Unknowns/Device.cs
@@ -1,35 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using Windows.Devices.Bluetooth;
-using Windows.Devices.Bluetooth.GenericAttributeProfile;
-using Idasen.BluetoothLE.Core.Interfaces.ServicesDiscovery;
-using Idasen.BluetoothLE.Core.Interfaces.ServicesDiscovery.Wrappers;
+﻿using System ;
+using System.Collections.Generic ;
+using System.Collections.Immutable ;
+using Windows.Devices.Bluetooth ;
+using Windows.Devices.Bluetooth.GenericAttributeProfile ;
+using Idasen.BluetoothLE.Core.Interfaces.ServicesDiscovery ;
+using Idasen.BluetoothLE.Core.Interfaces.ServicesDiscovery.Wrappers ;
 
 namespace Idasen.BluetoothLE.Characteristics.Characteristics.Unknowns
 {
     public class Device
         : IDevice
     {
-        public void Dispose()
+        public void Dispose ( )
         {
         }
 
-        public IObservable<BluetoothConnectionStatus> ConnectionStatusChanged { get; } = null;
-        public GattCommunicationStatus GattCommunicationStatus { get; } = GattCommunicationStatus.Unreachable;
-        public string Name { get; } = "Unknown Device";
-        public string DeviceId { get; } = "Unknown Device Id";
-        public bool IsPaired { get; } = false;
-        public BluetoothConnectionStatus ConnectionStatus { get; } = BluetoothConnectionStatus.Disconnected;
+        public IObservable < BluetoothConnectionStatus > ConnectionStatusChanged { get ; } = null ;
+        public GattCommunicationStatus GattCommunicationStatus { get ; } = GattCommunicationStatus.Unreachable ;
+        public string Name { get ; } = UnknownDeviceName ;
+        public string DeviceId { get ; } = UnknownDeviceId ; // todo maybe rename to just Id
+        public bool IsPaired { get ; } = false ;
+        public BluetoothConnectionStatus ConnectionStatus { get ; } = BluetoothConnectionStatus.Disconnected ;
 
-        public IReadOnlyDictionary<IGattDeviceServiceWrapper, IGattCharacteristicsResultWrapper> GattServices { get; } =
-            new Dictionary<IGattDeviceServiceWrapper, IGattCharacteristicsResultWrapper>().ToImmutableDictionary();
+        public IReadOnlyDictionary < IGattDeviceServiceWrapper , IGattCharacteristicsResultWrapper > GattServices
+        {
+            get ;
+        } = new Dictionary < IGattDeviceServiceWrapper , IGattCharacteristicsResultWrapper > ( ).ToImmutableDictionary ( ) ;
 
-        public IObservable<GattCommunicationStatus> GattServicesRefreshed { get; } = null;
+        public IObservable < GattCommunicationStatus > GattServicesRefreshed { get ; } = null ;
 
-        public void Connect()
+        public void Connect ( )
         {
             // do nothing
         }
+
+        internal const string UnknownDeviceName = "Unknown Device" ;
+        internal const string UnknownDeviceId   = "Unknown Device Id" ;
     }
 }
