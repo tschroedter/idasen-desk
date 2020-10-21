d7b8eca638a02fda40e25f5b38e9d2acca945c99
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-21 16:47:52 +1030

    add ReferenceOutputFactoryTests

---
+++ src/Idasen.BluetoothLE.Characteristics.Tests/Characteristics/Factories/ReferenceOutputFactoryTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions ;
+using Idasen.BluetoothLE.Characteristics.Characteristics.Factories ;
+using Idasen.BluetoothLE.Core.Interfaces.ServicesDiscovery ;
+using Selkie.AutoMocking ;
+
+namespace Idasen.BluetoothLE.Characteristics.Tests.Characteristics.Factories
+{
+    [ AutoDataTestClass ]
+    public class ReferenceOutputFactoryTests
+    {
+        [ AutoDataTestMethod ]
+        public void Create_ForInvoked_Instance (
+            ReferenceOutputFactory sut ,
+            IDevice                wrapper )
+        {
+            sut.Create ( wrapper )
+               .Should ( )
+               .NotBeNull ( ) ;
+        }
+    }
+}
