3dc25ccc60d418f0ae416eec4e07efa4c370089f
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-21 16:49:38 +1030

    add ReferenceInputFactoryConstructorNullTester

---
+++ src/Idasen.BluetoothLE.Characteristics.Tests/Characteristics/Factories/ConstructorNullTester/ReferenceInputFactoryConstructorNullTester.cs
@@ -0,0 +1,13 @@
+﻿using Idasen.BluetoothLE.Characteristics.Characteristics.Factories ;
+using Idasen.BluetoothLE.Core.Tests.DevicesDiscovery.ConstructorNullTesters ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+
+namespace Idasen.BluetoothLE.Characteristics.Tests.Characteristics.Factories.ConstructorNullTester
+{
+    [ TestClass ]
+    public class ReferenceInputFactoryConstructorNullTester
+        : BaseConstructorNullTester < ReferenceInputFactory >
+    {
+        public override int NumberOfConstructorsPassed { get ; } = 1 ;
+    }
+}
