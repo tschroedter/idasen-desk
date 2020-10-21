12ec1945268e7e23f5ca900d2cd6a2c189e7833c
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-21 11:29:27 +1030

    add ReferenceInputConstructorNullTester for Unknown

---
+++ src/Idasen.BluetoothLE.Characteristics.Tests/Characteristics/Unknowns/ConstructorNullTester/ReferenceInputConstructorNullTester.cs
@@ -0,0 +1,13 @@
+﻿using Idasen.BluetoothLE.Characteristics.Characteristics.Unknowns ;
+using Idasen.BluetoothLE.Core.Tests.DevicesDiscovery.ConstructorNullTesters ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+
+namespace Idasen.BluetoothLE.Characteristics.Tests.Characteristics.Unknowns.ConstructorNullTester
+{
+    [ TestClass ]
+    public class ReferenceInputConstructorNullTester
+        : BaseConstructorNullTester < ReferenceInput >
+    {
+        public override int NumberOfConstructorsPassed { get ; } = 0 ;
+    }
+}
