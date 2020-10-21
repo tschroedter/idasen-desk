584c41cc70c592c11ff7f343c1f4a4a3b60d1c0a
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-21 15:43:18 +1030

    add  GattCharacteristicsProviderFactoryTests

---
+++ src/Idasen.BluetoothLE.Characteristics.Tests/Characteristics/Customs/GattCharacteristicsProviderFactoryTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions ;
+using Idasen.BluetoothLE.Characteristics.Characteristics.Customs ;
+using Idasen.BluetoothLE.Core.Interfaces.ServicesDiscovery.Wrappers ;
+using Selkie.AutoMocking ;
+
+namespace Idasen.BluetoothLE.Characteristics.Tests.Characteristics.Customs
+{
+    [AutoDataTestClass]
+    public class GattCharacteristicsProviderFactoryTests
+    {
+        [ AutoDataTestMethod ]
+        public void Create_ForInvoked_Instance (
+            GattCharacteristicsProviderFactory sut,
+            IGattCharacteristicsResultWrapper wrapper)
+        {
+            sut.Create ( wrapper )
+               .Should ( )
+               .NotBeNull ( ) ;
+        }
+    }
+}
