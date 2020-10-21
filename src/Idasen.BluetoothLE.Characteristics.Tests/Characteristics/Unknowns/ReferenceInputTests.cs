a0cb9555fa55960a247f0ec4c160197cbe3ff2f6
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-21 11:28:01 +1030

    add ReferenceInputTests for Unknown

---
+++ src/Idasen.BluetoothLE.Characteristics.Tests/Characteristics/Unknowns/ReferenceInputTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions ;
+using Idasen.BluetoothLE.Characteristics.Characteristics.Unknowns ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+
+namespace Idasen.BluetoothLE.Characteristics.Tests.Characteristics.Unknowns
+{
+    [TestClass]
+    public class ReferenceInputTests
+    {
+        [TestMethod]
+        public void Constructor_ForInvoked_SetsCtrl1()
+        {
+            CreateSut().Ctrl1
+                       .Should()
+                       .BeEmpty();
+        }
+
+        private ReferenceInput CreateSut()
+        {
+            return new ReferenceInput();
+        }
+    }
+}
