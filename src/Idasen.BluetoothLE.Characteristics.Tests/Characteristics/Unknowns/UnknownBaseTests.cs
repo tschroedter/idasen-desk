275ebe269b595a613c79dd756b64d6c3577c9e79
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-21 11:59:13 +1030

    add UnknownBaseTests for Unknown

---
+++ src/Idasen.BluetoothLE.Characteristics.Tests/Characteristics/Unknowns/UnknownBaseTests.cs
@@ -0,0 +1,34 @@
+﻿using System ;
+using FluentAssertions ;
+using Idasen.BluetoothLE.Characteristics.Characteristics.Unknowns ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+
+namespace Idasen.BluetoothLE.Characteristics.Tests.Characteristics.Unknowns
+{
+    [ TestClass ]
+    public class UnknownBaseTests
+    {
+        [ TestMethod ]
+        public void Initialize_ForInvoked_DoesNothing ( )
+        {
+            Action action = ( ) => CreateSut ( ).Initialize < object > ( ) ;
+
+            action.Should ( )
+                  .NotThrow < Exception > ( ) ;
+        }
+
+        [ TestMethod ]
+        public void Refresh_ForInvoked_DoesNothing ( )
+        {
+            Action action = ( ) => CreateSut ( ).Refresh ( ) ;
+
+            action.Should ( )
+                  .NotThrow < Exception > ( ) ;
+        }
+
+        private UnknownBase CreateSut ( )
+        {
+            return new UnknownBase ( ) ;
+        }
+    }
+}
