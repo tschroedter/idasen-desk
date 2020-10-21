ba9e35691b1132917feeb632235ed991e04e849d
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-21 11:23:57 +1030

    add GenericAttributeTests for Unknown

---
+++ src/Idasen.BluetoothLE.Characteristics.Tests/Characteristics/Unknowns/GenericAttributeTests .cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions ;
+using Idasen.BluetoothLE.Characteristics.Characteristics.Unknowns ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+
+namespace Idasen.BluetoothLE.Characteristics.Tests.Characteristics.Unknowns
+{
+    [ TestClass ]
+    public class GenericAttributeTests
+    {
+        [ TestMethod ]
+        public void Constructor_ForInvoked_SetsRawDpg ( )
+        {
+            CreateSut ( ).RawServiceChanged
+                         .Should ( )
+                         .BeEmpty ( ) ;
+        }
+
+        private GenericAttribute CreateSut ( )
+        {
+            return new GenericAttribute ( ) ;
+        }
+    }
+}
