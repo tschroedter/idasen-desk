d2125c3aa8302915d526ed87743374e8c4011a8b
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-21 15:40:07 +1030

    add GattCharacteristicsProviderFactoryConstructorNullTester

---
+++ src/Idasen.BluetoothLE.Characteristics.Tests/Characteristics/Customs/ConstructorNullTester/DpgConstructorNullTester.cs
@@ -0,0 +1,13 @@
+﻿using Idasen.BluetoothLE.Characteristics.Characteristics.Customs ;
+using Idasen.BluetoothLE.Core.Tests.DevicesDiscovery.ConstructorNullTesters ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+
+namespace Idasen.BluetoothLE.Characteristics.Tests.Characteristics.Customs.ConstructorNullTester
+{
+    [ TestClass ]
+    public class GattCharacteristicsProviderFactoryConstructorNullTester
+        : BaseConstructorNullTester <GattCharacteristicsProviderFactory>
+    {
+        public override int NumberOfConstructorsPassed { get ; } = 1 ;
+    }
+}
