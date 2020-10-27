82ca4e39cf1f2e59e77bef765d5f40f0beed7514
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-28 09:32:08 +1030

    add InitialHeightProvider

---
+++ src/Idasen.BluetoothLE.Linak/Control/IInitialHeightProvider.cs
@@ -0,0 +1,35 @@
+﻿using System ;
+using System.Threading.Tasks ;
+
+namespace Idasen.BluetoothLE.Linak.Control
+{
+    /// <summary>
+    ///     The desk doesn't always provide the current height so we have to
+    ///     move the desk to get a reading.
+    /// </summary>
+    public interface IInitialHeightProvider
+        : IDisposable
+    {
+        void                 Initialize ( ) ;
+
+        /// <summary>
+        ///     Start the process of checking and getting the current height
+        ///     of the desk.
+        /// </summary>
+        /// <returns>
+        ///     A Task.
+        /// </returns>
+        Task                 Start ( ) ;
+
+        /// <summary>
+        ///     Notifies listeners when the provider was able to determine the
+        ///     height of the desk.
+        /// </summary>
+        IObservable < uint > Finished { get ; }
+
+        /// <summary>
+        ///     The current height of the desk.
+        /// </summary>
+        uint Height { get ; }
+    }
+}
---
+++ src/Idasen.BluetoothLE.Linak/Control/InitialHeightProvider.cs
@@ -0,0 +1,125 @@
+﻿using System ;
+using System.Reactive.Concurrency ;
+using System.Reactive.Linq ;
+using System.Reactive.Subjects ;
+using System.Threading.Tasks ;
+using Idasen.BluetoothLE.Characteristics.Common ;
+using Idasen.BluetoothLE.Core ;
+using Idasen.BluetoothLE.Linak.Interfaces ;
+using JetBrains.Annotations ;
+using Serilog ;
+
+namespace Idasen.BluetoothLE.Linak.Control
+{
+    /// <inheritdoc />
+    public class InitialHeightProvider
+        : IInitialHeightProvider
+    {
+        public InitialHeightProvider ( [ NotNull ] ILogger              logger ,
+                                       [ NotNull ] IScheduler           scheduler ,
+                                       [ NotNull ] IDeskHeightAndSpeed  heightAndSpeed ,
+                                       [ NotNull ] IDeskCommandExecutor executor ,
+                                       [ NotNull ] ISubject < uint >    subjectFinished )
+        {
+            Guard.ArgumentNotNull ( logger ,
+                                    nameof ( logger ) ) ;
+            Guard.ArgumentNotNull ( scheduler ,
+                                    nameof ( scheduler ) ) ;
+            Guard.ArgumentNotNull ( heightAndSpeed ,
+                                    nameof ( heightAndSpeed ) ) ;
+            Guard.ArgumentNotNull ( executor ,
+                                    nameof ( executor ) ) ;
+            Guard.ArgumentNotNull ( subjectFinished ,
+                                    nameof ( subjectFinished ) ) ;
+
+            _logger          = logger ;
+            _scheduler       = scheduler ;
+            _heightAndSpeed  = heightAndSpeed ;
+            _executor        = executor ;
+            _subjectFinished = subjectFinished ;
+        }
+
+        /// <inheritdoc />
+        public void Initialize ( )
+        {
+            _disposalHeightAndSpeed?.Dispose ( ) ;
+
+            _disposalHeightAndSpeed = _heightAndSpeed.HeightAndSpeedChanged
+                                                     .SubscribeOn ( _scheduler )
+                                                     .Subscribe ( OnHeightAndSpeedChanged ) ;
+
+            Height = _heightAndSpeed.Height ;
+        }
+
+        /// <inheritdoc />
+        public async Task Start ( )
+        {
+            if ( _disposalHeightAndSpeed == null )
+                throw new NotInitializeException ( "Initialize needs to be called first" ) ;
+
+            if ( _heightAndSpeed.Height > 0 )
+            {
+                _logger.Information ( $"Current height is {_heightAndSpeed.Height}" ) ;
+
+                _isReceivedHeightAndSpeed = true ;
+
+                return ;
+            }
+
+            _logger.Information ( "Trying to determine current height by moving the desk" ) ;
+
+            _isReceivedHeightAndSpeed = false ;
+
+            await _executor.Up ( ) ;
+            await _executor.Stop ( ) ;
+        }
+
+        /// <inheritdoc />
+        public IObservable < uint > Finished => _subjectFinished ;
+
+        /// <inheritdoc />
+        public void Dispose ( )
+        {
+            _disposalHeightAndSpeed?.Dispose ( ) ;
+        }
+
+        /// <inheritdoc />
+        public uint Height { get ; private set ; }
+
+        public delegate IInitialHeightProvider Factory ( IDeskCommandExecutor executor ,
+                                                         IDeskHeightAndSpeed  heightAndSpeed ) ;
+
+        private void OnHeightAndSpeedChanged ( HeightSpeedDetails details )
+        {
+            Height = details.Height ;
+
+            if ( _isReceivedHeightAndSpeed )
+                return ;
+
+            if ( details.Height <= 0 )
+            {
+                _logger.Information ( "Received invalid "             +
+                                      $"height {details.Height} and " +
+                                      $"speed {details.Speed} ..." ) ;
+
+                return ;
+            }
+
+            _subjectFinished.OnNext ( Height ) ;
+            _isReceivedHeightAndSpeed = true ;
+
+            _logger.Information ( "Received valid " +
+                                  $"height {details.Height}." ) ;
+        }
+
+        private readonly IDeskCommandExecutor _executor ;
+        private readonly IDeskHeightAndSpeed  _heightAndSpeed ;
+
+        private readonly ILogger           _logger ;
+        private readonly IScheduler        _scheduler ;
+        private readonly ISubject < uint > _subjectFinished ;
+
+        private IDisposable _disposalHeightAndSpeed ;
+        private bool        _isReceivedHeightAndSpeed ;
+    }
+}
