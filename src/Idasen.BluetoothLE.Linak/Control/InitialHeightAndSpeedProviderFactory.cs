3521216e0101e3162682c13496d909b7d2b1f23c
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-28 09:34:59 +1030

    add InitialHeightAndSpeedProviderFactory

---
+++ src/Idasen.BluetoothLE.Linak/Control/InitialHeightAndSpeedProviderFactory.cs
@@ -0,0 +1,33 @@
+﻿using Idasen.BluetoothLE.Core ;
+using Idasen.BluetoothLE.Linak.Interfaces ;
+using JetBrains.Annotations ;
+
+namespace Idasen.BluetoothLE.Linak.Control
+{
+    public class InitialHeightAndSpeedProviderFactory
+        : IInitialHeightAndSpeedProviderFactory
+    {
+        public InitialHeightAndSpeedProviderFactory ( [ NotNull ] InitialHeightProvider.Factory factory )
+        {
+            Guard.ArgumentNotNull ( factory ,
+                                    nameof ( factory ) ) ;
+
+            _factory = factory ;
+        }
+
+        public IInitialHeightProvider Create (
+            IDeskCommandExecutor executor ,
+            IDeskHeightAndSpeed  heightAndSpeed )
+        {
+            Guard.ArgumentNotNull ( executor ,
+                                    nameof ( executor ) ) ;
+            Guard.ArgumentNotNull ( heightAndSpeed ,
+                                    nameof ( heightAndSpeed ) ) ;
+
+            return _factory ( executor ,
+                              heightAndSpeed ) ;
+        }
+
+        private readonly InitialHeightProvider.Factory _factory ;
+    }
+}
---
+++ src/Idasen.BluetoothLE.Linak/Interfaces/IInitialHeightAndSpeedProviderFactory.cs
@@ -0,0 +1,11 @@
+﻿using Idasen.BluetoothLE.Linak.Control ;
+using JetBrains.Annotations ;
+
+namespace Idasen.BluetoothLE.Linak.Interfaces
+{
+    public interface IInitialHeightAndSpeedProviderFactory
+    {
+        IInitialHeightProvider Create ( [ NotNull ] IDeskCommandExecutor executor ,
+                                        [ NotNull ] IDeskHeightAndSpeed  heightAndSpeed ) ;
+    }
+}
