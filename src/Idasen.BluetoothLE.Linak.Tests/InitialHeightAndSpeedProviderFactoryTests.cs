41f141bfe274d376db12377592207499638401ab
tschroedter <Thomas.Schroedter@gmx.net>
2020-10-28 09:39:34 +1030

    add InitialHeightAndSpeedProviderFactoryTests

---
+++ src/Idasen.BluetoothLE.Linak.Tests/InitialHeightAndSpeedProviderFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions ;
+using Idasen.BluetoothLE.Linak.Control ;
+using Idasen.BluetoothLE.Linak.Interfaces ;
+using Selkie.AutoMocking ;
+
+namespace Idasen.BluetoothLE.Linak.Tests
+{
+    [ AutoDataTestClass ]
+    public class InitialHeightAndSpeedProviderFactoryTests
+    {
+        [ AutoDataTestMethod ]
+        public void Create_ForInvoked_Instance (
+            InitialHeightAndSpeedProviderFactory sut ,
+            IDeskCommandExecutor                 executor ,
+            IDeskHeightAndSpeed                  heightAndSpeed )
+        {
+            sut.Create ( executor ,
+                         heightAndSpeed )
+               .Should ( )
+               .NotBeNull ( ) ;
+        }
+    }
+}
