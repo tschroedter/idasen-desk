163bc81c91a9cd295e7e9b5308d71107d8cb8e0a
tschroedter <Thomas.Schroedter@gmx.net>
2020-11-03 15:13:16 +1030

    add RawValueToHeightAndSpeedConverterConstructorNullTests

---
+++ src/Idasen.BluetoothLE.Linak.Tests/ConstructorNullTester/RawValueToHeightAndSpeedConverterConstructorNullTests.cs
@@ -0,0 +1,12 @@
+﻿using Idasen.BluetoothLE.Core.Tests.DevicesDiscovery.ConstructorNullTesters ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+
+namespace Idasen.BluetoothLE.Linak.Tests.ConstructorNullTester
+{
+    [ TestClass ]
+    public class RawValueToHeightAndSpeedConverterConstructorNullTests
+        : BaseConstructorNullTester < RawValueToHeightAndSpeedConverter >
+    {
+        public override int NumberOfConstructorsPassed { get ; } = 1 ;
+    }
+}
