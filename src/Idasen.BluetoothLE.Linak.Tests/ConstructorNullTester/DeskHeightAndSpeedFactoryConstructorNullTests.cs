744c4d3d4987171d77ffdfff0da5a84d3ce2c0e3
tschroedter <Thomas.Schroedter@gmx.net>
2020-11-03 15:05:08 +1030

    add DeskHeightAndSpeedFactory

---
+++ src/Idasen.BluetoothLE.Linak.Tests/ConstructorNullTester/DeskHeightAndSpeedFactoryConstructorNullTests.cs
@@ -0,0 +1,12 @@
+﻿using Idasen.BluetoothLE.Core.Tests.DevicesDiscovery.ConstructorNullTesters ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+
+namespace Idasen.BluetoothLE.Linak.Tests.ConstructorNullTester
+{
+    [ TestClass ]
+    public class DeskHeightAndSpeedFactoryConstructorNullTests
+        : BaseConstructorNullTester < DeskHeightAndSpeedFactory >
+    {
+        public override int NumberOfConstructorsPassed { get ; } = 1 ;
+    }
+}
