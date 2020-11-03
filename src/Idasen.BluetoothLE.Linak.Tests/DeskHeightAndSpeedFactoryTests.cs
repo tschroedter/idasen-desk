3b6c055d874b566ac20e9a66bf59fd75f89544ab
tschroedter <Thomas.Schroedter@gmx.net>
2020-11-03 15:10:45 +1030

    add DeskHeightAndSpeedFactoryTests

---
+++ src/Idasen.BluetoothLE.Linak.Tests/DeskHeightAndSpeedFactoryTests.cs
@@ -0,0 +1,51 @@
+﻿using System ;
+using FluentAssertions ;
+using Idasen.BluetoothLE.Characteristics.Interfaces.Characteristics ;
+using Idasen.BluetoothLE.Linak.Interfaces ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+using NSubstitute ;
+
+namespace Idasen.BluetoothLE.Linak.Tests
+{
+    [ TestClass ]
+    public class DeskHeightAndSpeedFactoryTests
+    {
+        [ TestInitialize ]
+        public void Initialize ( )
+        {
+            _referenceOutput = Substitute.For < IReferenceOutput > ( ) ;
+            _factory         = TestFactory ;
+        }
+
+        [ TestMethod ]
+        public void Create_ForReferenceOutputNull_Throws ( )
+        {
+            Action action = ( ) => { CreateSut ( ).Create ( null! ) ; } ;
+
+            action.Should ( )
+                  .Throw < ArgumentNullException > ( )
+                  .WithParameter ( "referenceOutput" ) ;
+        }
+
+        [ TestMethod ]
+        public void Create_ForInvoked_ReturnsInstance ( )
+        {
+            CreateSut ( ).Create ( _referenceOutput )
+                         .Should ( )
+                         .NotBeNull ( ) ;
+        }
+
+        private IDeskHeightAndSpeed TestFactory ( IReferenceOutput _ )
+        {
+            return Substitute.For < IDeskHeightAndSpeed > ( ) ;
+        }
+
+        private DeskHeightAndSpeedFactory CreateSut ( )
+        {
+            return new DeskHeightAndSpeedFactory ( _factory ) ;
+        }
+
+        private DeskHeightAndSpeed.Factory _factory ;
+        private IReferenceOutput           _referenceOutput ;
+    }
+}
