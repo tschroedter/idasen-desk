818c1d8b19b0584213cfdf5fdb61737b6d4db97e
tschroedter <Thomas.Schroedter@gmx.net>
2020-11-03 14:43:36 +1030

    add DeskCommandExecutorFactoryConstructorNullTests

---
+++ src/Idasen.BluetoothLE.Linak.Tests/ConstructorNullTester/DeskCommandExecutorFactoryConstructorNullTests.cs
@@ -0,0 +1,13 @@
+﻿using Idasen.BluetoothLE.Core.Tests.DevicesDiscovery.ConstructorNullTesters ;
+using Idasen.BluetoothLE.Linak.Control ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+
+namespace Idasen.BluetoothLE.Linak.Tests.ConstructorNullTester
+{
+    [ TestClass ]
+    public class DeskCommandExecutorFactoryConstructorNullTests
+        : BaseConstructorNullTester < DeskCommandExecutorFactory >
+    {
+        public override int NumberOfConstructorsPassed { get ; } = 1 ;
+    }
+}
