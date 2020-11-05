2f4f2d722fe2dc44281441d7ab72ab6413f4c7bc
tschroedter <Thomas.Schroedter@gmx.net>
2020-11-05 11:53:09 +1030

    add DeskCharacteristicsConstructorNullTests

---
+++ src/Idasen.BluetoothLE.Linak.Tests/ConstructorNullTester/DeskCharacteristicsConstructorNullTests.cs
@@ -0,0 +1,12 @@
+﻿using Idasen.BluetoothLE.Core.Tests.DevicesDiscovery.ConstructorNullTesters ;
+using Microsoft.VisualStudio.TestTools.UnitTesting ;
+
+namespace Idasen.BluetoothLE.Linak.Tests.ConstructorNullTester
+{
+    [ TestClass ]
+    public class DeskCharacteristicsConstructorNullTests
+        : BaseConstructorNullTester < DeskCharacteristics >
+    {
+        public override int NumberOfConstructorsPassed { get ; } = 1 ;
+    }
+}
