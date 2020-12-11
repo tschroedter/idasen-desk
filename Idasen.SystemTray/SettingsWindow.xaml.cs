fd73d9232f26f4a63df298a1cf9f30d7bf989321
tschroedter <Thomas.Schroedter@gmx.net>
2020-12-11 21:07:54 +1030

    change the main window to settings window

---
+++ Idasen.SystemTray/SettingsWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Idasen.SystemTray
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
