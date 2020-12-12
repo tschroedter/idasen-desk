289485268ca193ed4c53b725c53d6dfd8073e96a
tschroedter <Thomas.Schroedter@gmx.net>
2020-12-12 13:50:42 +1030

    get a basic Settings Window displayed

---
+++ Idasen.SystemTray/SettingsConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Idasen.SystemTray
+{
+    public class SettingsConstants
+    {
+        public const uint DefaultHeightStandingInCm = 120 ;
+        public const uint DefaultHeightSeatingInCm  = 65 ;
+    }
+}
--- Idasen.SystemTray/SettingsWindow.xaml.cs
+++ Idasen.SystemTray/SettingsWindow.xaml.cs
@@ -1,13 +1,30 @@
-﻿namespace Idasen.SystemTray
+﻿using System.Windows.Input ;
+
+namespace Idasen.SystemTray
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class SettingsWindow
+        : ISettingsWindow
     {
-        public SettingsWindow()
+        public SettingsWindow ( )
         {
-            InitializeComponent();
+            InitializeComponent ( ) ;
         }
+
+        public uint StandingHeightInCm => _converter.TryConvertToUInt ( Standing.Value ,
+                                                                        SettingsConstants.DefaultHeightStandingInCm ) ;
+
+        public uint SeatingHeightInCm => _converter.TryConvertToUInt ( Seating.Value ,
+                                                                       SettingsConstants.DefaultHeightSeatingInCm ) ;
+
+        private void ImageClose_MouseDown ( object               sender ,
+                                            MouseButtonEventArgs e )
+        {
+            Close ( ) ;
+        }
+
+        private readonly IDoubleToUIntConverter _converter = new DoubleToUIntConverter ( ) ;
     }
 }
