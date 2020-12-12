2f3053c0b95c0c6747b22afe83dfa6e2c789351b
tschroedter <Thomas.Schroedter@gmx.net>
2020-12-12 16:55:56 +1030

    add Idasen.Launcher project

---
+++ src/Idasen.Launcher/CallerEnricher.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics ;
+using System.Linq ;
+using Serilog ;
+using Serilog.Core ;
+using Serilog.Events ;
+
+namespace Idasen.Launcher
+{
+    public class CallerEnricher : ILogEventEnricher
+    {
+        public void Enrich ( LogEvent                 logEvent ,
+                             ILogEventPropertyFactory propertyFactory )
+        {
+            var skip = 3 ;
+            while ( true )
+            {
+                var stack = new StackFrame ( skip ) ;
+                if ( ! stack.HasMethod ( ) )
+                {
+                    logEvent.AddPropertyIfAbsent ( new LogEventProperty ( "Caller" ,
+                                                                          new ScalarValue ( "<unknown method>" ) ) ) ;
+                    return ;
+                }
+
+                var method = stack.GetMethod ( ) ;
+
+                if ( method               != null &&
+                     method.DeclaringType != null )
+                    if ( method.DeclaringType.Assembly != typeof ( Log ).Assembly )
+                    {
+                        var caller =
+                            $"{method.DeclaringType.FullName}.{method.Name}({string.Join ( ", " , method.GetParameters ( ).Select ( pi => pi.ParameterType.FullName ) )})" ;
+                        logEvent.AddPropertyIfAbsent ( new LogEventProperty ( "Caller" ,
+                                                                              new ScalarValue ( caller ) ) ) ;
+                        return ;
+                    }
+
+                skip ++ ;
+            }
+        }
+    }
+}
---
+++ src/Idasen.Launcher/ContainerProvider.cs
@@ -0,0 +1,65 @@
+﻿using System ;
+using System.IO ;
+using Autofac ;
+using AutofacSerilogIntegration ;
+using Idasen.BluetoothLE.Core ;
+using Idasen.BluetoothLE.Linak ;
+using Serilog ;
+using Serilog.Events ;
+
+namespace Idasen.Launcher
+{
+    public static class ContainerProvider
+    {
+        public static IContainer Create ( string appName ,
+                                          string appLogFileName )
+        {
+            var logFolder = CreateFullPathSettingsFolderName ( appName ) ;
+            var logFile   = CreateFullPathSettingsFileName(appName, appLogFileName);
+
+            if ( ! Directory.Exists ( logFolder ) )
+                Directory.CreateDirectory ( logFolder ) ;
+
+            const string template =
+                // "[{Timestamp:HH:mm:ss.ffff} {Level:u3}] {Message}{NewLine}{Exception}" ;
+                "[{Timestamp:HH:mm:ss.ffff} {Level:u3}] {Message} (at {Caller}){NewLine}{Exception}";
+
+            Log.Logger = new LoggerConfiguration ( )
+                        .Enrich.WithCaller ( )
+                        .MinimumLevel.Information ( )
+                        .WriteTo
+                        .ColoredConsole ( LogEventLevel.Debug ,
+                                          template )
+                        .WriteTo
+                        .File ( logFile )
+                        .CreateLogger ( ) ;
+
+            var builder = new ContainerBuilder ( ) ;
+
+            builder.RegisterLogger ( ) ;
+            builder.RegisterModule < BluetoothLECoreModule > ( ) ;
+            builder.RegisterModule < BluetoothLELinakModule > ( ) ;
+
+            return builder.Build ( ) ;
+        }
+
+#pragma warning disable SecurityIntelliSenseCS // MS Security rules violation
+        public static string CreateFullPathSettingsFileName ( string appName ,
+                                                              string fileName )
+        {
+            var fullPath = Path.Combine ( CreateFullPathSettingsFolderName ( appName ) ,
+                                          fileName ) ;
+            return fullPath ;
+        }
+
+        private static string CreateFullPathSettingsFolderName ( string appName )
+        {
+            var appData = Environment.GetFolderPath ( Environment.SpecialFolder.CommonApplicationData) ;
+            var folderName = Path.Combine ( appData ,
+                                            appName ) ;
+
+            return folderName ;
+        }
+#pragma warning restore SecurityIntelliSenseCS // MS Security rules violation
+    }
+}
