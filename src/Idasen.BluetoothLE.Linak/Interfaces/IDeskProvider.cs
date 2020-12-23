e111c97dc589182a92647906c0c1a28cb162f6e0
tschroedter <Thomas.Schroedter@gmx.net>
2020-12-23 16:27:32 +1030

    add summaries for IDeskProvider

--- src/Idasen.BluetoothLE.Linak/Interfaces/IDeskProvider.cs
+++ src/Idasen.BluetoothLE.Linak/Interfaces/IDeskProvider.cs
@@ -7,19 +7,43 @@ namespace Idasen.BluetoothLE.Linak.Interfaces
     public interface IDeskProvider
         : IDisposable
     {
-        /// todo
+        /// <summary>
+        ///     Fires when a desk was detected.
+        /// </summary>
         IObservable < IDesk > DeskDetected { get ; }
 
-        /// todo
+        /// <summary>
+        ///     Initialize the instance and is required to be called first.
+        /// </summary>
+        /// <returns>
+        ///     Returns itself.
+        /// </returns>
         IDeskProvider Initialize ( ) ;
 
-        /// todo
+        /// <summary>
+        ///     Start the desk detection process.
+        /// </summary>
+        /// <returns>
+        ///     Returns itself.
+        /// </returns>
         IDeskProvider StartDetecting ( ) ;
 
-        /// todo
+        /// <summary>
+        ///     Stops the desk detection process.
+        /// </summary>
+        /// <returns>
+        ///     Returns itself.
+        /// </returns>
         IDeskProvider StopDetecting() ;
 
-        /// todo
+        /// <summary>
+        ///     Try to detect a desk until the token has been cancelled.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns>
+        ///     A tuple with the first value indicating if a desk was found or not.
+        ///     The second parameter is the detected desk or null.
+        /// </returns>
         Task<(bool, IDesk)> TryGetDesk(CancellationToken token) ;
     }
 }
