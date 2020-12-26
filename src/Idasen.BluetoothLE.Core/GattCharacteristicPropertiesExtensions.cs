6d15dff5dc8ffd9026c5cdd0091838a47bdd6145
tschroedter <Thomas.Schroedter@gmx.net>
2020-12-26 20:12:18 +1030

    add GattCharacteristicPropertiesExtensions

---
+++ src/Idasen.BluetoothLE.Core/GattCharacteristicPropertiesExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System ;
+using System.Collections.Generic ;
+using System.Diagnostics.CodeAnalysis ;
+using Windows.Devices.Bluetooth.GenericAttributeProfile ;
+
+namespace Idasen.BluetoothLE.Core
+{
+    [ExcludeFromCodeCoverage]
+    public static class GattCharacteristicPropertiesExtensions
+    {
+        /// <summary>
+        ///     Converts the <see cref="GattCharacteristicProperties"/> into a
+        ///     string containing the property names separated by comma.
+        /// </summary>
+        /// <param name="properties">
+        ///     This <see cref="GattCharacteristicProperties"/> to convert into a string.
+        /// </param>
+        /// <returns>
+        ///     A string containing the property names separated by a comma.
+        /// </returns>
+        public static string ToCsv(this GattCharacteristicProperties properties)
+        {
+            var list = new List<GattCharacteristicProperties>();
+
+            foreach (GattCharacteristicProperties property in
+                Enum.GetValues(typeof(GattCharacteristicProperties)))
+            {
+                if ((properties & property) == property) list.Add(property);
+            }
+
+            return string.Join(", ",
+                               list);
+        }
+    }
+}
