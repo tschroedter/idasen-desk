cca22c7fb01cd2321341111808a9ec2d160fd84a
tschroedter <Thomas.Schroedter@gmx.net>
2020-12-31 10:40:35 +1030

    add DefaultDeviceMonitoringTimeout constant

--- src/Idasen.SystemTray/Utils/Constants.cs
+++ src/Idasen.SystemTray/Utils/Constants.cs
@@ -5,9 +5,10 @@
         public const string ApplicationName  = "Idasen.SystemTray" ;
         public const string SettingsFileName = "Settings.json" ;
 
-        public const uint   DefaultHeightStandingInCm = 120 ;
-        public const uint   DefaultHeightSeatingInCm  = 65 ;
-        public const string DefaultDeviceName         = "Desk" ;
-        public const ulong  DefaultDeviceAddress      = 250635178951455u ;
+        public const uint   DefaultHeightStandingInCm      = 120 ;
+        public const uint   DefaultHeightSeatingInCm       = 65 ;
+        public const string DefaultDeviceName              = "Desk" ;
+        public const ulong  DefaultDeviceAddress           = 250635178951455u ;
+        public const uint   DefaultDeviceMonitoringTimeout = 600 ; // in seconds
     }
 }
