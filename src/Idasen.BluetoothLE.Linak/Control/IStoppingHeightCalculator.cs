377b82fd2d7093dfd1f785f02aa1d1b93fb2ae60
tschroedter <Thomas.Schroedter@gmx.net>
2021-01-03 17:25:17 +1030

    add missing XML summaries

--- src/Idasen.BluetoothLE.Linak/Control/IStoppingHeightCalculator.cs
+++ src/Idasen.BluetoothLE.Linak/Control/IStoppingHeightCalculator.cs
@@ -2,42 +2,73 @@
 {
     public interface IStoppingHeightCalculator
     {
-        /// <inheritdoc />
+        /// <summary>
+        ///     The expected movement in mm of the desk from maximum speed to zero
+        ///     speed.
+        /// </summary>
         uint MaxSpeedToStopMovement { get ; set ; }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     The maximum movement in mm speed of the desk.
+        /// </summary>
         int MaxSpeed { get ; set ; }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     The current movement speed of the desk.
+        /// </summary>
         int Speed { get ; set ; }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     The Fudge factor used to calculate the stopping height.
+        /// </summary>
         float FudgeFactor { get ; set ; }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     The target height in mm to reach by the desk.
+        /// </summary>
         uint TargetHeight { get ; set ; }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     The current height of the desk.
+        /// </summary>
         uint Height { get ; set ; }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     Delta between the TargetHeight and StoppingHeight
+        /// </summary>
         uint Delta { get ; }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     The estimated stopping height of the desk in motion.
+        /// </summary>
         uint StoppingHeight { get ; }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     The estimated movement of the desk until it stops.
+        /// </summary>
         int MovementUntilStop { get ; }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     Indicates if the current desk height is available or not.
+        /// </summary>
         bool HasReachedTargetHeight { get ; }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     Move the desk into the given direction.
+        /// </summary>
         Direction MoveIntoDirection { get ; set ; }
 
+        /// <summary>
+        ///     Direction to start moving into.
+        /// </summary>
         Direction StartMovingIntoDirection { get ; set ; }
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     Calculate the get only properties: Delta, StoppingHeight...
+        /// </summary>
+        /// <returns>
+        ///     Itself.
+        /// </returns>
         IStoppingHeightCalculator Calculate ( ) ;
     }
 }
