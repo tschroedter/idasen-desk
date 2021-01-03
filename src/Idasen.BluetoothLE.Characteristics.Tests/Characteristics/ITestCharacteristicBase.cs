92498c8a14b9fae8a713829f3f48c809bcf1a686
tschroedter <Thomas.Schroedter@gmx.net>
2021-01-03 17:09:06 +1030

    remove unused value in test

--- src/Idasen.BluetoothLE.Characteristics.Tests/Characteristics/ITestCharacteristicBase.cs
+++ src/Idasen.BluetoothLE.Characteristics.Tests/Characteristics/ITestCharacteristicBase.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic ;
-using Idasen.BluetoothLE.Characteristics.Interfaces.Characteristics ;
+﻿using Idasen.BluetoothLE.Characteristics.Interfaces.Characteristics ;
 using Idasen.BluetoothLE.Core.Interfaces.ServicesDiscovery ;
 
 namespace Idasen.BluetoothLE.Characteristics.Tests.Characteristics
@@ -8,7 +7,5 @@ namespace Idasen.BluetoothLE.Characteristics.Tests.Characteristics
         : ICharacteristicBase
     {
         delegate ITestCharacteristicBase Factory ( IDevice device ) ;
-
-        IEnumerable < byte > RawValue { get ; }
     }
 }
