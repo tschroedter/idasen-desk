41cf2dd36de44a8d8f7aa400a44d91436c248788
tschroedter <Thomas.Schroedter@gmx.net>
2021-01-09 06:25:26 +1030

    update DeskCharacteristicsCreator to use ICharacteristicBaseFactory

--- src/Idasen.BluetoothLE.Linak/DeskCharacteristicsCreator.cs
+++ src/Idasen.BluetoothLE.Linak/DeskCharacteristicsCreator.cs
@@ -1,4 +1,6 @@
-﻿using Idasen.BluetoothLE.Characteristics.Interfaces.Characteristics.Factories ;
+﻿using Idasen.BluetoothLE.Characteristics.Characteristics.Factories ;
+using Idasen.BluetoothLE.Characteristics.Interfaces.Characteristics ;
+using Idasen.BluetoothLE.Characteristics.Interfaces.Characteristics.Factories ;
 using Idasen.BluetoothLE.Core ;
 using Idasen.BluetoothLE.Core.Interfaces.ServicesDiscovery ;
 using Idasen.BluetoothLE.Linak.Interfaces ;
@@ -12,36 +14,16 @@ namespace Idasen.BluetoothLE.Linak
         : IDeskCharacteristicsCreator
     {
         public DeskCharacteristicsCreator (
-            [ NotNull ] ILogger                  logger ,
-            [ NotNull ] IGenericAccessFactory    genericAccessFactory ,
-            [ NotNull ] IGenericAttributeFactory genericAttributeFactory ,
-            [ NotNull ] IReferenceInputFactory   referenceInputFactory ,
-            [ NotNull ] IReferenceOutputFactory  referenceOutputFactory ,
-            [ NotNull ] IDpgFactory              dpgFactory ,
-            [ NotNull ] IControlFactory          controlFactory )
+            [ NotNull ] ILogger                    logger ,
+            [ NotNull ] ICharacteristicBaseFactory baseFactory )
         {
             Guard.ArgumentNotNull ( logger ,
                                     nameof ( logger ) ) ;
-            Guard.ArgumentNotNull ( genericAccessFactory ,
-                                    nameof ( genericAccessFactory ) ) ;
-            Guard.ArgumentNotNull ( genericAttributeFactory ,
-                                    nameof ( genericAttributeFactory ) ) ;
-            Guard.ArgumentNotNull ( referenceInputFactory ,
-                                    nameof ( referenceInputFactory ) ) ;
-            Guard.ArgumentNotNull ( referenceOutputFactory ,
-                                    nameof ( referenceOutputFactory ) ) ;
-            Guard.ArgumentNotNull ( dpgFactory ,
-                                    nameof ( dpgFactory ) ) ;
-            Guard.ArgumentNotNull ( controlFactory ,
-                                    nameof ( controlFactory ) ) ;
+            Guard.ArgumentNotNull ( baseFactory ,
+                                    nameof ( baseFactory ) ) ;
 
-            _logger                  = logger ;
-            _genericAccessFactory    = genericAccessFactory ;
-            _genericAttributeFactory = genericAttributeFactory ;
-            _referenceInputFactory   = referenceInputFactory ;
-            _referenceOutputFactory  = referenceOutputFactory ;
-            _dpgFactory              = dpgFactory ;
-            _controlFactory          = controlFactory ;
+            _logger      = logger ;
+            _baseFactory = baseFactory ;
         }
 
         /// <inheritdoc />
@@ -53,31 +35,24 @@ namespace Idasen.BluetoothLE.Linak
                                     nameof ( device ) ) ;
             Guard.ArgumentNotNull ( characteristics ,
                                     nameof ( characteristics ) ) ;
-            Guard.ArgumentNotNull ( device ,
-                                    nameof ( device ) ) ;
 
             _logger.Debug ( $"[{device.Id}] Creating desk characteristics {characteristics}" ) ;
 
             characteristics.WithCharacteristics ( DeskCharacteristicKey.GenericAccess ,
-                                                  _genericAccessFactory.Create ( device ) )
+                                                  _baseFactory.Create < IGenericAccess > ( device ) )
                            .WithCharacteristics ( DeskCharacteristicKey.GenericAttribute ,
-                                                  _genericAttributeFactory.Create ( device ) )
+                                                  _baseFactory.Create < IGenericAttribute > ( device ) )
                            .WithCharacteristics ( DeskCharacteristicKey.ReferenceInput ,
-                                                  _referenceInputFactory.Create ( device ) )
+                                                  _baseFactory.Create < IReferenceInput > ( device ) )
                            .WithCharacteristics ( DeskCharacteristicKey.ReferenceOutput ,
-                                                  _referenceOutputFactory.Create ( device ) )
+                                                  _baseFactory.Create < IReferenceOutput > ( device ) )
                            .WithCharacteristics ( DeskCharacteristicKey.Dpg ,
-                                                  _dpgFactory.Create ( device ) )
+                                                  _baseFactory.Create < IDpg > ( device ) )
                            .WithCharacteristics ( DeskCharacteristicKey.Control ,
-                                                  _controlFactory.Create ( device ) ) ;
+                                                  _baseFactory.Create < IControl > ( device ) ) ;
         }
 
-        private readonly IControlFactory          _controlFactory ;
-        private readonly IDpgFactory              _dpgFactory ;
-        private readonly IGenericAccessFactory    _genericAccessFactory ;
-        private readonly IGenericAttributeFactory _genericAttributeFactory ;
-        private readonly ILogger                  _logger ;
-        private readonly IReferenceInputFactory   _referenceInputFactory ;
-        private readonly IReferenceOutputFactory  _referenceOutputFactory ;
+        private readonly ICharacteristicBaseFactory _baseFactory ;
+        private readonly ILogger                    _logger ;
     }
 }
