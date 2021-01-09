ae6d68700dff56881f3e35b8575713b446ac5726
tschroedter <Thomas.Schroedter@gmx.net>
2021-01-10 10:10:02 +1030

    updated ContainerProvider to use LoggerProvider

--- src/Idasen.Launcher/ContainerProvider.cs
+++ src/Idasen.Launcher/ContainerProvider.cs
@@ -1,46 +1,22 @@
-﻿using System ;
-using System.Collections.Generic ;
-using System.IO ;
+﻿using System.Collections.Generic ;
 using Autofac ;
 using Autofac.Core ;
 using AutofacSerilogIntegration ;
 using Idasen.BluetoothLE.Core ;
 using Idasen.BluetoothLE.Linak ;
 using Serilog ;
-using Serilog.Events ;
 
 namespace Idasen.Launcher
 {
     public static class ContainerProvider
     {
-        public static IContainer Create ( string                  appName ,
-                                          string                  appLogFileName ,
-                                          IEnumerable < IModule > otherModules = null )
+        public static IContainer Create (
+            string                  appName ,
+            string                  appLogFileName ,
+            IEnumerable < IModule > otherModules = null )
         {
-            var logFolder = CreateFullPathApplicationFolderName ( appName ) ;
-            var logFile = CreateFullPathLogFileName ( appName ,
-                                                      appLogFileName ) ;
-
-            if ( ! Directory.Exists ( logFolder ) )
-                Directory.CreateDirectory ( logFolder ) ;
-
-            const string template =
-                // "[{Timestamp:HH:mm:ss.ffff} {Level:u3}] {Message}{NewLine}{Exception}" ;
-                "[{Timestamp:HH:mm:ss.ffff} {Level:u3}] {Message} (at {Caller}){NewLine}{Exception}" ;
-
-            Log.Logger = new LoggerConfiguration ( )
-                        .MinimumLevel
-                        .Debug ( )
-                        .Enrich
-                        .WithCaller ( )
-                        .WriteTo
-                        .ColoredConsole ( LogEventLevel.Debug ,
-                                          template )
-                        .WriteTo
-                        .File ( logFile ,
-                                LogEventLevel.Debug ,
-                                template )
-                        .CreateLogger ( ) ;
+            Log.Logger = LoggerProvider.CreateLogger ( appName ,
+                                                       appLogFileName ) ;
 
             var builder = new ContainerBuilder ( ) ;
 
@@ -48,32 +24,15 @@ namespace Idasen.Launcher
             builder.RegisterModule < BluetoothLECoreModule > ( ) ;
             builder.RegisterModule < BluetoothLELinakModule > ( ) ;
 
-            if ( otherModules != null )
-                foreach ( var otherModule in otherModules )
-                {
-                    builder.RegisterModule ( otherModule ) ;
-                }
+            if ( otherModules == null )
+                return builder.Build ( ) ;
+
+            foreach ( var otherModule in otherModules )
+            {
+                builder.RegisterModule ( otherModule ) ;
+            }
 
             return builder.Build ( ) ;
         }
-
-#pragma warning disable SecurityIntelliSenseCS // MS Security rules violation
-        public static string CreateFullPathLogFileName ( string appName ,
-                                                              string fileName )
-        {
-            var fullPath = Path.Combine ( CreateFullPathApplicationFolderName ( appName ) ,
-                                          fileName ) ;
-            return fullPath ;
-        }
-
-        private static string CreateFullPathApplicationFolderName ( string appName )
-        {
-            var appData = Environment.GetFolderPath ( Environment.SpecialFolder.CommonApplicationData ) ;
-            var folderName = Path.Combine ( appData ,
-                                            appName ) ;
-
-            return folderName ;
-        }
-#pragma warning restore SecurityIntelliSenseCS // MS Security rules violation
     }
 }
