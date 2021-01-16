29e15d3221170be1377c3fcf9668803e3bf2fa49
tschroedter <Thomas.Schroedter@gmx.net>
2021-01-17 10:04:52 +1030

    add LogBluetoothStatusException to Exception extensions

---
+++ src/Idasen.BluetoothLE.Characteristics/Common/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Idasen.BluetoothLE.Characteristics.Common
+{
+    public class Constants
+    {
+        public const string CheckAndEnableBluetooth = "Make sure Bluetooth is enabled!";
+    }
+}
--- src/Idasen.BluetoothLE.Characteristics/Common/ExceptionExtensions.cs
+++ src/Idasen.BluetoothLE.Characteristics/Common/ExceptionExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using System ;
+using Idasen.BluetoothLE.Core ;
+using JetBrains.Annotations ;
+using Serilog ;
 
 namespace Idasen.BluetoothLE.Characteristics.Common
 {
@@ -10,6 +13,20 @@ namespace Idasen.BluetoothLE.Characteristics.Common
                    ((uint)e.HResult == 0x8007048F ||
                     (uint)e.HResult == 0x800710DF ||
                     (uint)e.HResult == 0x8000FFFF);
+        }
+
+        public static void LogBluetoothStatusException ( this          Exception exception ,
+                                                         [ NotNull ]   ILogger   log ,
+                                                         [ CanBeNull ] string    message = "" )
+        {
+            Guard.ArgumentNotNull ( log ,
+                                    nameof ( log ) ) ;
+
+            var text = Constants.CheckAndEnableBluetooth +
+                       $" (0x{exception?.HResult:X}) " +
+                       message;
+
+            log.Information ( text ) ;
         }
     }
 }
