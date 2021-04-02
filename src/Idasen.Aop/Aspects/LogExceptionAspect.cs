2ee53debf5410c7a69eb0e5bd30846a9a06637fd
tschroedter <Thomas.Schroedter@gmx.net>
2021-04-02 18:13:22 +1030

    add LogExceptionAspect

---
+++ src/Idasen.Aop/Aspects/LogExceptionAspect.cs
@@ -0,0 +1,35 @@
+﻿using System ;
+using Castle.DynamicProxy ;
+using Idasen.Aop.Interfaces ;
+using JetBrains.Annotations ;
+using Serilog ;
+
+namespace Idasen.Aop.Aspects
+{
+    public class LogExceptionAspect : IInterceptor
+    {
+        // todo move ErrorManager to common space
+        public LogExceptionAspect([NotNull] ILogger                    logger,
+                                  [NotNull] IInvocationToTextConverter converter)
+        {
+            _logger    = logger;
+            _converter = converter;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            try
+            {
+                invocation.Proceed ( ) ;
+            }
+            catch ( Exception exception )
+            {
+                _logger.Error ( $"{_converter.Convert ( invocation )} " +
+                                exception ) ;
+            }
+        }
+
+        private readonly IInvocationToTextConverter _converter;
+        private readonly ILogger                    _logger;
+    }
+}
