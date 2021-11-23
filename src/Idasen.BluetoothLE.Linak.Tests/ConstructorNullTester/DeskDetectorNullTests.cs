918171f134fb16171b859fafd26806cbbe0ca1b3
ajoe bcool <Joe Cool>
2021-11-23 20:00:46 +1030

    add DeskDetectorNullTests

---
+++ src/Idasen.BluetoothLE.Linak.Tests/ConstructorNullTester/DeskDetectorNullTests.cs
@@ -0,0 +1,12 @@
+﻿using Idasen.BluetoothLE.Common.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Idasen.BluetoothLE.Linak.Tests.ConstructorNullTester
+{
+    [TestClass]
+    public class DeskDetectorNullTests
+        : BaseConstructorNullTester<Desk>
+    {
+        public override int NumberOfConstructorsPassed { get; } = 1;
+    }
+}
