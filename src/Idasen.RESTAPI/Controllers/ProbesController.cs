d47915432038bfbc3357e3648cf68046c2fc2b7f
tschroedter <Thomas.Schroedter@gmx.net>
2021-12-25 17:41:16 +1030

    add ProbeController supporting liveness and readiness

---
+++ src/Idasen.RESTAPI/Controllers/ProbesController.cs
@@ -0,0 +1,30 @@
+﻿using Idasen.RESTAPI.Filters ;
+using Idasen.RESTAPI.Interfaces ;
+using Microsoft.AspNetCore.Mvc ;
+
+namespace Idasen.RESTAPI.Controllers
+{
+    [ ApiKeyAuth ]
+    [ Route ( "desk/" ) ]
+    public class ProbeController : ControllerBase
+    {
+        public ProbeController( IDeskManager               manager )
+        {
+            _manager    = manager ;
+        }
+
+        [ Route ("liveness") ]
+        public IActionResult GetLiveness( )
+        {
+            return Ok ( true ) ;
+        }
+
+        [ Route ("readiness") ]
+        public IActionResult GetReadiness( )
+        {
+            return Ok ( _manager.IsReady ) ;
+        }
+
+        private readonly IDeskManager        _manager ;
+    }
+}
