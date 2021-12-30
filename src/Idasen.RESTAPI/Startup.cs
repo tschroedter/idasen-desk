5f6c079d6f121fcf466e4335f0fd95105f2d307a
tschroedter <Thomas.Schroedter@gmx.net>
2021-12-30 15:14:26 +1030

    add more logging

--- src/Idasen.RESTAPI/Startup.cs
+++ src/Idasen.RESTAPI/Startup.cs
@@ -3,6 +3,7 @@ using FluentValidation.AspNetCore ;
 using Idasen.RESTAPI.Desks ;
 using Idasen.RESTAPI.Filters ;
 using Idasen.RESTAPI.Interfaces ;
+using Idasen.RESTAPI.Repositories ;
 using Microsoft.AspNetCore.Builder ;
 using Microsoft.Extensions.Configuration ;
 using Microsoft.Extensions.DependencyInjection ;
