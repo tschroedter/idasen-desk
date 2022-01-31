060371c640be11be2af96954d61fc48452db5546
tschroedter <Thomas.Schroedter@gmx.net>
2022-01-31 20:17:01 +1030

    add IsAllowedToMove property to interface

--- src/Idasen.BluetoothLE.Linak/Interfaces/IDeskMover.cs
+++ src/Idasen.BluetoothLE.Linak/Interfaces/IDeskMover.cs
@@ -6,10 +6,11 @@ namespace Idasen.BluetoothLE.Linak.Interfaces
     public interface IDeskMover
         : IDisposable
     {
-        uint                 Height       { get ; }
-        int                  Speed        { get ; }
-        uint                 TargetHeight { get ; set ; }
-        IObservable < uint > Finished     { get ; }
+        uint                 Height          { get ; }
+        int                  Speed           { get ; }
+        uint                 TargetHeight    { get ; set ; }
+        IObservable < uint > Finished        { get ; }
+        bool                 IsAllowedToMove { get ; }
         Task < bool >        Up ( ) ;
         Task < bool >        Down ( ) ;
         Task < bool >        Stop ( ) ;
