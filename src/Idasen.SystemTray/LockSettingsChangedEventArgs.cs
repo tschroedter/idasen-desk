2b73cf1ad037f3e8c5ab217a0cc0f2119130277b
tschroedter <Thomas.Schroedter@gmx.net>
2022-02-03 20:02:14 +1030

    add parental lock to System.Tray

---
+++ src/Idasen.SystemTray/LockSettingsChangedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System ;
+
+namespace Idasen.SystemTray
+{
+    public class LockSettingsChangedEventArgs
+        : EventArgs
+    {
+        public bool IsLocked { get ; }
+
+        public LockSettingsChangedEventArgs ( bool IsLocked )
+        {
+            this.IsLocked = IsLocked ;
+        }
+    }
+}
