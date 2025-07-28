c718685c36fddac08d5ff44e244c6c9da361d68d
thomas <Thomas.Schroedter@gmx.net>
2025-07-28 10:49:00 +0930

    add ApplicationThemeManager

---
+++ src/Idasen.SystemTray.Win11/Utils/IApplicationThemeManager.cs
@@ -0,0 +1,10 @@
+﻿using Wpf.Ui.Appearance;
+
+namespace Idasen.SystemTray.Win11.Utils ;
+
+public interface IApplicationThemeManager
+{
+    public ApplicationTheme GetAppTheme ( ) ;
+
+    public void Apply ( ApplicationTheme theme ) ;
+}
---
+++ src/Idasen.SystemTray.Win11/Utils/MyApplicationThemeManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis ;
+using Wpf.Ui.Appearance;
+
+namespace Idasen.SystemTray.Win11.Utils ;
+
+[ExcludeFromCodeCoverage]
+public class MyApplicationThemeManager : IApplicationThemeManager
+{
+    public ApplicationTheme GetAppTheme ( ) =>
+        ApplicationThemeManager.GetAppTheme ( ) ;
+
+    public void Apply ( ApplicationTheme theme ) => 
+        ApplicationThemeManager.Apply ( theme ) ;
+}
