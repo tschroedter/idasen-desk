1a44ae290b623c29f239ea46533a966cdd8da94e
thomas <Thomas.Schroedter@gmx.net>
2025-09-18 16:10:53 +0930

    move the log file to ProgramData folder

--- src/Idasen.SystemTray.Win11/Utils/AppLoggerProvider.cs
+++ src/Idasen.SystemTray.Win11/Utils/AppLoggerProvider.cs
@@ -1,3 +1,4 @@
+using System.IO ;
 using Idasen.Launcher ;
 using Serilog ;
 
@@ -10,13 +11,29 @@ public static class AppLoggerProvider
     ///     loaded from the appsettings.json file. The using the file will crash the application without any hint why after
     ///     a few seconds starting it. The workaround is to configure the logger programmatically for now.
     ///     (related class: Idasen.Launcher.LoggerProvider)
-    ///
     ///     ToDo: This needs to be fixed properly later and involves create a crash dump file.
     /// </summary>
     /// <param name="appName"></param>
     /// <returns></returns>
     public static ILogger CreateLogger ( string appName )
     {
+        // PSEUDOCODE:
+        // - Determine ProgramData base path via Environment.SpecialFolder.CommonApplicationData.
+        // - Create vendor/app-specific logs directory: ProgramData\Idasen\<appName>\logs.
+        // - Ensure the directory exists.
+        // - Build full log file path with Constants.LogFilename.
+        // - Use this path for Serilog file sink.
+
+        var programDataBase = Environment.GetFolderPath ( Environment.SpecialFolder.CommonApplicationData ) ;
+        var logsDirectory = Path.Combine ( programDataBase ,
+                                           "Idasen" ,
+                                           appName ,
+                                           "logs" ) ;
+        Directory.CreateDirectory ( logsDirectory ) ;
+
+        var logFilePath = Path.Combine ( logsDirectory ,
+                                         Constants.LogFilename ) ;
+
         return new LoggerConfiguration ( )
               .Enrich.WithProperty ( "Application" ,
                                      appName )
@@ -27,7 +44,7 @@ public static class AppLoggerProvider
               .Enrich.WithMachineName ( )
               .Enrich.WithEnvironmentUserName ( )
               .Enrich.WithCaller ( ) // requires package: Serilog.Enrichers.WithCaller
-              .WriteTo.Async ( w => w.File ( @".\logs\" + Constants.LogFilename,
+              .WriteTo.Async ( w => w.File ( logFilePath ,
                                              outputTemplate :
                                              "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] [ProcessId:{ProcessId}] [ThreadId:{ThreadId}] [Class:{SourceContext}] [Method:{CallerMemberName}] {Message}{NewLine}{Exception}" ,
                                              rollingInterval : RollingInterval.Day ,
