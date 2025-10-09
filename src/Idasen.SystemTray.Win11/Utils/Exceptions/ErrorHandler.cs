478d18e441000668337702933535ff9bfe77e8fa
thomas <Thomas.Schroedter@gmx.net>
2025-10-09 14:34:42 +1030

    fix SonarCube warning: Refactor the containing loop to do more than one iteration.

--- src/Idasen.SystemTray.Win11/Utils/Exceptions/ErrorHandler.cs
+++ src/Idasen.SystemTray.Win11/Utils/Exceptions/ErrorHandler.cs
@@ -14,12 +14,9 @@ public class ErrorHandler
     // should be last as it handles any exception
     public void Handle ( Exception exception , ILogger logger )
     {
-        foreach ( var handler in _handlers.Where(handler => handler.CanHandle(exception)) )
-        {
-            handler.Handle ( exception ,
-                             logger ) ;
+        var handler = _handlers.FirstOrDefault ( handler => handler.CanHandle ( exception ) ) ;
 
-            return ;
-        }
+        handler?.Handle ( exception ,
+                          logger ) ;
     }
 }
